70153bf0b9f1a07b99edad23cb1f2ed178799ec8
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 13:20:01 +0530

    Day2Assignment

---
+++ product/src/Testproduct.java
@@ -0,0 +1,19 @@
+
+public class Testproduct {
+		public static void main(String[] args) {
+		 Product product=new Product();
+			product.setProductCode("P101");
+			product.setProductName("Laptop");
+			product.setProducPrice(45000.0);
+				
+				System.out.println("ProductCode  :" +  product.getproductcode());
+				System.out.println("ProductName  :" +  product.getproductname());
+				System.out.println("ProductPrice :" + product.getproductprice());
+				
+		
+	}
+ 	
+		
+}
+
+
