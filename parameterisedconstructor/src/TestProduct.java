3bbcf56f210ea1864e4fca8a772239a5e0a9475b
Erukulla Pravalika <erukullapravalika2911999@gmail.com>
2021-04-17 13:22:19 +0530

    Day2Assignment

---
+++ parameterisedconstructor/src/TestProduct.java
@@ -0,0 +1,11 @@
+
+public class TestProduct {
+	
+public static void main(String[] args) {
+ Product product=new Product("p101" , "Laptop", 45000.00);
+	product.displayProductDetails();
+		// TODO Auto-generated method stub
+
+	}
+
+}
